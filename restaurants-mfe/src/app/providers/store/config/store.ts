af200bb6c969d198a08047b0e5787407021ab16b
Matveeva Svetlana <sveta.matvik.sveta@gmail.com>
2023-04-22 16:10:47 +0300

    feat(restaurants-mfe): add combineReducers

--- restaurants-mfe/src/app/providers/store/config/store.ts
+++ restaurants-mfe/src/app/providers/store/config/store.ts
@@ -1,14 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import { counterReducer } from 'entities/counter'
 
 import { type IStateSchema } from '../types'
 
 export const createReduxStore = (initialState?: IStateSchema) => {
+    const rootReducers = combineReducers<IStateSchema>({
+        counter: counterReducer
+    })
+
     return configureStore<IStateSchema>({
-        reducer: {
-            counter: counterReducer
-        },
+        reducer: rootReducers,
         devTools: __IS_DEV__,
         preloadedState: initialState
     })
